bce76c7802c6904eca5497ff9dac0ca4dc919f3c
srimantahyland <srimanta.singh@hyland.com>
2021-10-02 11:15:52 +0530

    REST EndPoint

---
+++ Schema/ProductSchema.cs
@@ -0,0 +1,16 @@
+﻿using GraphQlProject.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQlProject.Schema
+{
+    public class ProductSchema : GraphQL.Types.Schema
+    {
+        public ProductSchema(ProductQuery productQuery)
+        {
+            Query = productQuery;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,63 @@
+using GraphQL.Types;
+using GraphQlProject.Interfaces;
+using GraphQlProject.Query;
+using GraphQlProject.Schema;
+using GraphQlProject.Services;
+using GraphQlProject.Type;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQlProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddTransient<IProduct, ProductService>();
+            services.AddSingleton<ProductType>();
+            services.AddSingleton<ProductQuery>();
+            services.AddSingleton<ISchema, ProductSchema>();
+
+            services.AddGraphQL(options =>
+            {
+                options.EnableMetrics = false;
+            }).AddSystemTextJson();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
